d8ef696d991e83a3f9d04821cd38ab616456caca
hamdalla2 <hamdalla2@yahoo.com>
2020-09-20 16:41:10 +0300

    hi

--- script.js
+++ script.js
@@ -3,46 +3,63 @@ var obj = {}
 obj.tom = []
 obj.sam = []
 obj.kat = []
+var obj2 = {}
+obj2.tom = []
+obj2.sam = []
+obj2.kat = []
+
+function each(array,f){
+	if(!Array.isArray(array)){
+    for(var i in array){
+    	f(array[i],i)
+    }
+	}
+	else{for(var i=0;i<array.length;i++){f(array[i],i)}}
+}
+
 function s() {
     var name = $('#userwelcom').text().split(' ')
     var Activeuser = name[1].toLowerCase()
     var value = $('#txt').val()
     console.log(Activeuser)
-    if (Activeuser === 'user') { alert('No user Selected') }
+    if (Activeuser === 'user') { alert('No user selected!!!') }
     else {
         if (value === '') {
-            alert('Add Smoe Tasks!!')
+            alert('Add Some Tasks!!!')
         }
         else {
-            var newtask = $('<div calss=tasks></div>')
+            var newtask = $('<div class=tasks></div>')
             var timeadd = Date().split(' ')
             timeadd.pop(); timeadd.pop(); timeadd.pop(); timeadd.pop()
-
-            newtask.html("<input type=checkbox onclick=t(this)>" + value + "<input type=button value=Edit onclick=r(this)><input type=button value=Delete onclick=d(this)><br>" + timeadd.join(' '))
-            $('#two').append(newtask)
+            newtask.html("<input type=checkbox onclick=t(this)>" + value + "<input type=button value=Edit onclick=r(this)><input type=button value=Delete onclick=d(this)>Added on:" + timeadd.join(' '))
             $('#txt').val("")
-            obj[Activeuser].push(value)
+            obj[Activeuser].push(newtask)
+            $('#two').html("")
+            for(var i=0;i<obj[Activeuser].length;i++){
+            $('#two').append(obj[Activeuser][i])}
         }
     }
 
 }
 // Hamdallah driver Qamar navigator
 function t(e) {
-    var x = e.parentElement
+	var name = $('#userwelcom').text().split(' ')
+    var Activeuser = name[1].toLowerCase()
+	var x = e.parentElement
+	var c=document.getElementsByClassName("tasks")
+	each(c,function(item,i){if(item===x)
+	obj[Activeuser].splice(i,1)})
+	obj2[Activeuser].push(x)
     if ($(e).is(':checked')) {
-        $('#three').append(x)
+            obj2[Activeuser].push(newtask)
+            $('#two').html("")
+            for(var i=0;i<obj[Activeuser].length;i++){
+            $('#two').append(obj[Activeuser][i])}
         $(x).css('text-decoration', 'line-through black')
-        // obj[Activeuser][done].pop
-        // obj[Activeuser][done].push(value)
-
-
     }
     else {
         $('#two').append(x)
         $(x).css('text-decoration', 'none')
-        // obj[Activeuser][notdone].push(value)
-        // obj[Activeuser][done].pop()
-
     }
 }
 
