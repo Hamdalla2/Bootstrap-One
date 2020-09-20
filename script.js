a2212c191050d1495ce9a99ddb63574f94b6c061
hamdalla2 <hamdalla2@yahoo.com>
2020-09-20 12:17:57 +0300

    nice

--- script.js
+++ script.js
@@ -2,6 +2,39 @@
 function s(){
      var value =$('#txt').val()
      var newtask =$('<div></div>')
-     newtask.html(value)
+     newtask.html("<input type=checkbox onclick=t(this)>"+value+"<input type=button value=Edit onclick=r(this)><input type=button value=Delete onclick=d(this)>")
      $('#two').append(newtask)
+     $('#txt').val("")
 }
+// Hamdallah driver Qamar navigator
+function t(e){
+	var x=e.parentElement
+	if ($(e).is(':checked')){
+	$('#three').append(x)}
+	else{$('#two').append(x)}
+}
+
+function d(e){
+	var x=e.parentElement
+	x.remove()
+}
+
+function r(e){
+	var y=e.parentElement.firstChild
+	var x=e.parentElement
+	if($(y).is(':checked')){
+	x.innerHTML="<input type=checkbox onclick=t(this) checked><input type=\"text\" id=txt2><input type='button' value='Save' onclick=w(this)><input type=button value=Delete onclick=d(this)>"
+	}
+	else{
+	x.innerHTML="<input type=checkbox onclick=t(this)><input type=\"text\" id=txt2><input type='button' value='Save' onclick=w(this)><input type=button value=Delete onclick=d(this)>"
+}}
+
+function w(e){
+	var y=e.parentElement.firstChild
+	var x=e.parentElement
+	if($(y).is(':checked')){
+	x.innerHTML="<input type=checkbox onclick=t(this) checked>"+$("#txt2").val()+"<input type='button' value='Edit' onclick=r(this)><input type=button value=Delete onclick=d(this)>"
+	}
+	else{
+    x.innerHTML="<input type=checkbox onclick=t(this)>"+$("#txt2").val()+"<input type='button' value='Edit' onclick=r(this)><input type=button value=Delete onclick=d(this)>"
+}}
